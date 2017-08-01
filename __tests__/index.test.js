da6c2b0af8cebf31d57bb7800e258523ca182f17
Miguel Palau <mpalauzarza@gmail.com>
2017-08-01 11:47:06 -0500

    making styled-components work

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,14 +1,23 @@
 import { shallow } from 'enzyme'
 import React from 'react'
 import renderer from 'react-test-renderer'
+import 'jest-styled-components'
 
 import App from '../pages/index.js'
 
 describe('With Enzyme', () => {
   it('App shows "Hello world!"', () => {
     const app = shallow(<App />)
+    expect(app.dive().find('h1').text()).toEqual('Hello World!')
+  })
+})
 
-    expect(app.find('h1').text()).toEqual('Hello World!')
+describe('with jest-styled-components', () => {
+  const component = renderer.create(<App />)
+  const tree = component.toJSON()
+  it('works with styled-components', () => {
+    expect(tree).toMatchSnapshot()
+    expect(tree).toHaveStyleRule('color', 'red')
   })
 })
 
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
-export default () => (
-  <h1>Hello World!</h1>
-)
+import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: red;
+  font-size: 50px;
+`
+
+export default () => <Title>Hello World!</Title>
