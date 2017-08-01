88e71b82aa6eda55ef6a90158b13cf560fa0dfa2
Miguel Palau <mpalauzarza@gmail.com>
2017-08-01 15:06:35 -0500

    svg example

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
+import Trust from '../assets/trust.svg'
 
 const Title = styled.h1`
   color: red;
   font-size: 50px;
 `
 
-export default () => <Title>Hello World!</Title>
+const SVG = styled.div`
+  max-width: 600px;
+  & > svg {
+    max-width: 100%;
+  }
+`
+
+export default () => (
+  <div>
+    <Title>Hello World!</Title>
+    <SVG>
+      <Trust />
+    </SVG>
+  </div>
+)
