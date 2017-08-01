f8f84725ec46f58d52145185a5163908ac27995f
Miguel Palau <mpalauzarza@gmail.com>
2017-08-01 16:35:00 -0500

    optimized svgs

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,7 @@
-import React from 'react'
 import styled from 'styled-components'
-import Trust from '../assets/trust.svg'
 
 const Title = styled.h1`
   color: red;
   font-size: 50px;
 `
-
-const SVG = styled.div`
-  max-width: 600px;
-  & > svg {
-    max-width: 100%;
-  }
-`
-
-export default () => (
-  <div>
-    <Title>Hello World!</Title>
-    <SVG>
-      <Trust />
-    </SVG>
-  </div>
-)
+export default () => <Title>Hello World!</Title>
