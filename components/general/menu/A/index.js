a5c10f6e21192d75b8eb64a5a6c373dc5ff46ff5
Miguel Palau <mpalauzarza@gmail.com>
2017-08-02 15:54:42 -0500

    the power of mixins but in JS

--- components/general/menu/A/index.js
+++ components/general/menu/A/index.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components'
 import { rem } from 'polished'
+import { fontFamily } from '%/styles/fonts'
 
 export default styled.a`
   display: block;
   color: #ffffff;
   font-size: ${rem(14)};
-  font-family: 'roboto';
+  font-family: ${fontFamily};
   font-weight: 400;
 `
