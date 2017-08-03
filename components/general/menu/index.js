e710dc82ad43d6697ecb713dd5c895dd2fee53b9
Miguel Palau <mpalauzarza@gmail.com>
2017-08-03 11:06:20 -0500

    nice menu animation

--- components/general/menu/index.js
+++ components/general/menu/index.js
@@ -1,7 +1,18 @@
 import InnerMenu from './InnerMenu'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import colors from '%/styles/colors'
 import PropTypes from 'prop-types'
+
+const appear = keyframes`
+  from {
+    transform: translateY(-100%);
+  }
+  to {
+    transform: translateY(0);
+  }
+`
+
+const animation = `${appear} 0.5s ease`
 
 const Menu = styled.nav`
   background-color: ${props => props.active ? colors.blue : 'transparent'};
@@ -10,6 +21,7 @@ const Menu = styled.nav`
   left: 0;
   width: 100%;
   transition: 0.2s;
+  animation: ${props => props.active ? animation : 'none'};
 `
 Menu.propTypes = {
   active: PropTypes.bool
