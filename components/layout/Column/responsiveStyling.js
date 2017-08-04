e5a7807c7f4e539615ec0d014f4ffc9fc1d17928
Miguel Palau <mpalauzarza@gmail.com>
2017-08-04 12:23:46 -0500

    moar docs

--- components/layout/Column/responsiveStyling.js
+++ components/layout/Column/responsiveStyling.js
@@ -1,13 +1,26 @@
 import { mediaMin } from '%/styles/mixins'
 import { grid, bpKeysArray } from '%/styles'
 
+/**
+ * Take number and cssKey receive percetage
+ * @param {Number} num
+ * @param {string} cssKey
+ *
+ * @returns {string}
+ */
 const percentageCreator = (num, cssKey) => {
   return num
     ? `${cssKey}: ${(num / grid.columns) * 100}%;`
     : undefined
 }
 
-// Magic!
+/**
+ * Takes props from the breakpoints Object and turns them
+ * into columns and offsets of that breakpoint
+ *
+ * @param {Object} props
+ * @returns {string}
+ */
 const responsiveStyling = props => {
   const usableBreaks = bpKeysArray.filter(bp => props[bp]) // ['laptop']
   return usableBreaks.map(bp => mediaMin[bp]`
