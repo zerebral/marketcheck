ae48acef438d27a3e483aa994ed7743f3894e1cf
Miguel Palau <mpalauzarza@gmail.com>
2017-08-08 13:50:04 -0500

    stars component done

--- components/Home/Testimonials/Statements/Statement/index.js
+++ components/Home/Testimonials/Statements/Statement/index.js
@@ -1,9 +1,14 @@
 import styled from 'styled-components'
 import { rem } from 'polished'
 
+import Rating from './Rating'
+
 const Statement = styled.div`
   padding: 0 ${rem(15)};
+  flex-basis: ${1 / 3 * 100}%;
 `
 
 export default () =>
-  <Statement>Soon  Statement</Statement>
+  <Statement>
+    <Rating />
+  </Statement>
