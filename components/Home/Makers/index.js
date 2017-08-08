8824be5b5d1c77a7af5db4d0a8986af7fe6d52da
Miguel Palau <mpalauzarza@gmail.com>
2017-08-08 17:07:10 -0500

    done with the makers title

--- components/Home/Makers/index.js
+++ components/Home/Makers/index.js
@@ -2,9 +2,27 @@ import styled from 'styled-components'
 import { em } from 'polished'
 import { colors } from '%/styles'
 
+import { H4 } from '~/general'
+import { Column, Row, Container } from '~/layout'
+
 const Makers = styled.div`
   padding: ${em(70)} 0 ${em(50)};
   background-color: ${colors.softgray};
+  text-align: center;
+  color: ${colors.softblue};
 `
 
-export default Makers
+const M4 = H4.extend`
+  margin-bottom: ${em(40)};
+`
+
+export default () =>
+  <Makers>
+    <Container>
+      <Row>
+        <Column laptop={{ cols: 10, offset: 1 }}>
+          <M4>Popular Makers</M4>
+        </Column>
+      </Row>
+    </Container>
+  </Makers>
