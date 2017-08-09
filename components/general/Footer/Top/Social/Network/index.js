8088fad6504f29d245ad274b6e916bcd94fcf015
Miguel Palau <mpalauzarza@gmail.com>
2017-08-09 13:39:54 -0500

    top of footer done

--- components/general/Footer/Top/Social/Network/index.js
+++ components/general/Footer/Top/Social/Network/index.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import { rem } from 'polished'
 import { percentage } from '%/styles/mixins'
+import { colors } from '%/styles'
 
 const Network = styled.a`
   display: block;
@@ -8,12 +9,17 @@ const Network = styled.a`
   margin-bottom: ${rem(50)};
   max-width: ${percentage(1, 3)};
   flex-basis: ${percentage(1, 3)};
+  &:hover, &:active, &:focus {
+    color: ${colors.white};
+  }
 
   & > svg {
     height: ${rem(20)};
     width: ${rem(20)};
     margin: 0 auto;
-    fill: currentColor;
+    & [fill] {
+      fill: currentColor;
+    }
   }
 `
 
