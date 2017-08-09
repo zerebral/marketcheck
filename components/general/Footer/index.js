afb902114e7ee5dc3d40929dadf84d25f58f3ca9
Miguel Palau <mpalauzarza@gmail.com>
2017-08-09 11:22:21 -0500

    footer social svgs added

--- components/general/Footer/index.js
+++ components/general/Footer/index.js
@@ -1,12 +1,23 @@
 import styled from 'styled-components'
 import { colors } from '%/styles'
+import { em, rgba } from 'polished'
+
+import { Container, Row, Column } from '~/layout'
 
 const Footer = styled.footer`
   background-color: ${colors.darkblue};
-  color: ${colors.white};
-  text-align: center;
-  padding-top: 75px;
-  padding-bottom: 55px;
+  color: ${rgba(colors.white, 0.56)};
+  padding-top: ${em(75)};
+  padding-bottom: ${em(55)};
 `
 
-export default Footer
+export default () =>
+  <Footer>
+    <Container>
+      <Row>
+        <Column laptop={{ cols: 10, offset: 1 }}>
+          <h1>Such foots</h1>
+        </Column>
+      </Row>
+    </Container>
+  </Footer>
