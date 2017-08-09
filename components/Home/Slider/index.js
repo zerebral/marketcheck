068bd60517c5fc05d9f1fc596921d9edc046aed4
Miguel Palau <mpalauzarza@gmail.com>
2017-08-09 02:45:04 -0500

    adding more slides

--- components/Home/Slider/index.js
+++ components/Home/Slider/index.js
@@ -2,16 +2,14 @@ import Slider from 'react-slick'
 
 const SliderSettings = {
   dots: true,
-  infinite: true,
-  speed: 2000,
+  arrows: true,
+  autoplay: true,
   slidesToShow: 1,
   slidesToScroll: 1
 }
 
 export default () =>
   <Slider {...SliderSettings}>
-    <div><h3>1</h3></div>
-    <div><h3>2</h3></div>
-    <div><h3>3</h3></div>
-    <div><h3>4</h3></div>
+    <div>Hello world</div>
+    <div>Bye world</div>
   </Slider>
