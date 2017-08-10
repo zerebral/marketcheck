97df7f646a6b0db0824041eb1cd3585972cc1ea8
Miguel Palau <mpalauzarza@gmail.com>
2017-08-10 12:53:19 -0500

    review code for #11

--- components/home/Features/index.js
+++ components/home/Features/index.js
@@ -5,14 +5,14 @@ import Title from './Title'
 import Icons from './Icons'
 
 const Features = styled.div`
-  padding: ${em(70)} 0;
+  padding: ${em(60)} 0;
 `
 
 export default () =>
   <Features>
     <Container>
       <Row>
-        <Column laptop={{ cols: 8, offset: 2 }}>
+        <Column laptop={{ cols: 8, offset: 2 }} phablet={{ cols: 10, offset: 1 }}>
           <Title />
           <Icons />
         </Column>
