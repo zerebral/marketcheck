7d31e490d989851e5d26f32681cf44f9bf2b1f86
Miguel Palau <mpalauzarza@gmail.com>
2017-08-11 10:50:10 -0500

    i heard you like cache?
    and mudkips :)
    
    now there is SSR-rendering cache even with params answers

---
+++ server/getCacheKey.js
@@ -0,0 +1,9 @@
+/*
+ * NB: make sure to modify this to take into account anything that should trigger
+ * an immediate page change (e.g a locale stored in req.session)
+ */
+function getCacheKey (req) {
+  return `${req.url}`
+}
+
+module.exports = getCacheKey
