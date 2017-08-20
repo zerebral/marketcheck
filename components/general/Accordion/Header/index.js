1412081db1c985a2f7537f1cf269cd391b4dfda8
Miguel Palau <mpalauzarza@gmail.com>
2017-08-20 14:59:11 -0500

    more styling on the accordions

--- components/general/Accordion/Header/index.js
+++ components/general/Accordion/Header/index.js
@@ -1,20 +1,21 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { colors } from '%/styles'
-import { rem } from 'polished'
-
-const conditionalPadding = props =>
-  props.open
-    ? `${rem(15)} ${rem(30)}`
-    : `${rem(15)}`
+import { rem, rgba } from 'polished'
 
 const Header = styled.div`
   cursor: pointer;
-  background-color: ${props => props.open ? colors.blue : '#333'};
+  background-color: #333;
   color: ${colors.white};
-  padding: ${conditionalPadding};
+  padding: ${rem(15)};
   display: flex;
   align-items: center;
   transition: 0.3s;
+  border-bottom: 1px solid ${rgba('#d6d6d6', 0.3)};
+  ${props => props.open && css`
+    background-color: ${colors.blue};
+    padding: ${rem(15)} ${rem(30)};
+    border-bottom-color: transparent;
+  `}
 `
 
 export default Header
