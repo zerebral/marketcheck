cbc138b32f060b34cbb80de927c44f5e4dc8986b
Miguel Palau <mpalauzarza@gmail.com>
2017-08-21 01:23:59 -0500

    attempting to have a scatter data

--- components/VDP/Contents/index.js
+++ components/VDP/Contents/index.js
@@ -4,25 +4,14 @@ import { Accordion } from '~/general'
 
 import SummaryReport from './SummaryReport'
 import VehicleInformation from './VehicleInformation'
+import MarketComparison from './MarketComparison'
 
 export default props =>
   <ContentsWrapper>
     <Container>
       <SummaryReport />
       <VehicleInformation />
-      <Accordion title='Market Comparison'>
-        Such accordant wow Commodo vitae, aliquam ut, posuere eget, dui.
-        Praesent massa dui, mattis et, vehicula auctor, iaculis id, diam. Morbi
-        viverra neque sit amet risus. Nunc pellentesque aliquam orci. Proin
-        neque elit, mollis vel, tristique nec, varius consectetuer, lorem. Nam
-        malesuada ornare nunc. Duis turpis turpis, fermentum a, aliquet quis,
-        sodales at, dolor. Duis eget velit eget risus fringilla hendrerit.
-        Nulla facilisi. Mauris turpis pede, aliquet ac, mattis sed, consequat
-        in, massa. Cum sociis natoque penatibus et magnis dis parturient
-        montes, nascetur ridiculus mus. Etiam egestas posuere metus. Aliquam
-        erat volutpat. Donec non tortor. Vivamus posuere nisi mollis dolor.
-        Quisque porttitor nisi ac.
-      </Accordion>
+      <MarketComparison />
       <Accordion title='Averages Over Time'>
         Such accordant wow Commodo vitae, aliquam ut, posuere eget, dui.
         Praesent massa dui, mattis et, vehicula auctor, iaculis id, diam. Morbi
