e1c2e7419d50ff726343820636eb682b99011f12
Miguel Palau <mpalauzarza@gmail.com>
2017-09-01 02:30:25 -0500

    circle graphs looking prettier

--- components/VDP/Contents/TimeOnMarket/CircleGraphs.js
+++ components/VDP/Contents/TimeOnMarket/CircleGraphs.js
@@ -1,53 +1,9 @@
-import {
-  ResponsiveContainer,
-  PieChart,
-  Pie,
-  Legend
-} from 'recharts'
-import { colors } from '%/styles'
 import CircleGraphWrapper from './CircleGraphsWrapper'
-
-import styled from 'styled-components'
-
-const thisCar = [
-  {name: 'Rest of year', value: 176},
-  {name: 'Listed for sale', value: 189}
-]
-
-const LegendWrapper = styled.div`
-  text-align: center;
-`
-
-const BlueText = styled.p`
-  color: ${colors.blue};
-`
-const NormalText = styled.p`
-  color: ${colors.gray};
-`
-
-const CustoLegend = (props) =>
-  <LegendWrapper>
-    <BlueText>189 days</BlueText>
-    <NormalText>This Car</NormalText>
-  </LegendWrapper>
+import ChartMaker from './ChartMaker.js'
 
 export default () =>
   <CircleGraphWrapper>
-    <ResponsiveContainer width='100%' height={165} >
-      <PieChart>
-        <Pie
-          data={thisCar}
-          nameKey='name'
-          valueKey='value'
-          outerRadius={80}
-          innerRadius={60}
-          fill='#dae0e8'
-          activeIndex={1}
-          activeShape={{ fill: colors.blue }}
-          startAngle={90}
-          endAngle={450}
-        />
-        <Legend content={CustoLegend} />
-      </PieChart>
-    </ResponsiveContainer>
+    <ChartMaker days={189} text='This Car' />
+    <ChartMaker days={245} text='Local Average' />
+    <ChartMaker days={302} text='National Average' />
   </CircleGraphWrapper>
