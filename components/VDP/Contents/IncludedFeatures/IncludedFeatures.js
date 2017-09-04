5763391fe8782b6244e06e8439a003e93921e6cf
Miguel Palau <mpalauzarza@gmail.com>
2017-09-04 15:03:11 -0500

    IncludedFeatures mobile done

--- components/VDP/Contents/IncludedFeatures/IncludedFeatures.js
+++ components/VDP/Contents/IncludedFeatures/IncludedFeatures.js
@@ -1,23 +1,37 @@
 import { Accordion } from '~/general'
 import styled from 'styled-components'
 
+import Feature from './Feature.js'
+
+import {
+  Air,
+  Bluetooth,
+  Brake,
+  Cam,
+  Geo,
+  Keyless,
+  Wheels
+} from './Icons'
+
 const AtAGlance = styled.p`
   opacity: 0.54;
   margin-bottom: 40px !important;
 `
 
+const Features = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
 export default () =>
-  <Accordion title='Included Features' open >
+  <Accordion title='Included Features'>
     <AtAGlance>12 Similarly Equiped Carts Curently Available</AtAGlance>
-    Such accordant wow Commodo vitae, aliquam ut, posuere eget, dui.
-    Praesent massa dui, mattis et, vehicula auctor, iaculis id, diam. Morbi
-    viverra neque sit amet risus. Nunc pellentesque aliquam orci. Proin
-    neque elit, mollis vel, tristique nec, varius consectetuer, lorem. Nam
-    malesuada ornare nunc. Duis turpis turpis, fermentum a, aliquet quis,
-    sodales at, dolor. Duis eget velit eget risus fringilla hendrerit.
-    Nulla facilisi. Mauris turpis pede, aliquet ac, mattis sed, consequat
-    in, massa. Cum sociis natoque penatibus et magnis dis parturient
-    montes, nascetur ridiculus mus. Etiam egestas posuere metus. Aliquam
-    erat volutpat. Donec non tortor. Vivamus posuere nisi mollis dolor.
-    Quisque porttitor nisi ac.
+    <Features>
+      <Feature icon={Bluetooth} name='Bluetooth' />
+      <Feature icon={Cam} name='Rear Cam' />
+      <Feature icon={Wheels} name='Alloy wheels' />
+      <Feature icon={Geo} name='Navigation' />
+      <Feature icon={Air} name='Air Conditioner' />
+      <Feature icon={Brake} name='Brake Assistance' />
+      <Feature icon={Keyless} name='Keyless Start' />
+    </Features>
   </Accordion>
