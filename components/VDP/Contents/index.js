7671c9fd7438bedb99bae10fc9c18a048a0ff93d
Miguel Palau <mpalauzarza@gmail.com>
2017-09-11 14:57:31 -0500

    skeleton for Competition component

--- components/VDP/Contents/index.js
+++ components/VDP/Contents/index.js
@@ -11,6 +11,7 @@ import IncludedFeatures from './IncludedFeatures'
 import FuelEfficiency from './FuelEfficiency'
 import Environmental from './Environmental'
 import ModelPopularity from './ModelPopularity'
+import Competition from './Competition'
 
 export default props =>
   <ContentsWrapper>
@@ -24,19 +25,7 @@ export default props =>
       <FuelEfficiency />
       <Environmental />
       <ModelPopularity />
-      <Accordion title='The Competition'>
-        Such accordant wow Commodo vitae, aliquam ut, posuere eget, dui.
-        Praesent massa dui, mattis et, vehicula auctor, iaculis id, diam. Morbi
-        viverra neque sit amet risus. Nunc pellentesque aliquam orci. Proin
-        neque elit, mollis vel, tristique nec, varius consectetuer, lorem. Nam
-        malesuada ornare nunc. Duis turpis turpis, fermentum a, aliquet quis,
-        sodales at, dolor. Duis eget velit eget risus fringilla hendrerit.
-        Nulla facilisi. Mauris turpis pede, aliquet ac, mattis sed, consequat
-        in, massa. Cum sociis natoque penatibus et magnis dis parturient
-        montes, nascetur ridiculus mus. Etiam egestas posuere metus. Aliquam
-        erat volutpat. Donec non tortor. Vivamus posuere nisi mollis dolor.
-        Quisque porttitor nisi ac.
-      </Accordion>
+      <Competition />
       <Accordion title='Model Resale Value'>
         Such accordant wow Commodo vitae, aliquam ut, posuere eget, dui.
         Praesent massa dui, mattis et, vehicula auctor, iaculis id, diam. Morbi
