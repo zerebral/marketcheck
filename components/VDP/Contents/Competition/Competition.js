2b435c52758c0631ebdc6129e9cb6acca6a35c91
Miguel Palau <mpalauzarza@gmail.com>
2017-09-11 17:42:41 -0500

    Averages done

--- components/VDP/Contents/Competition/Competition.js
+++ components/VDP/Contents/Competition/Competition.js
@@ -1,9 +1,10 @@
 import { Accordion } from '~/general'
 import styled from 'styled-components'
+import Averages from './Averages.js'
 
 const Summary = styled.p`
   opacity: 0.54;
-  margin-bottom: 60px !important;
+  margin-bottom: 25px !important;
 `
 
 export default () =>
@@ -11,15 +12,5 @@ export default () =>
     <Summary>
       Similar cars currently for sale
     </Summary>
-    Such accordant wow Commodo vitae, aliquam ut, posuere eget, dui.
-    Praesent massa dui, mattis et, vehicula auctor, iaculis id, diam. Morbi
-    viverra neque sit amet risus. Nunc pellentesque aliquam orci. Proin
-    neque elit, mollis vel, tristique nec, varius consectetuer, lorem. Nam
-    malesuada ornare nunc. Duis turpis turpis, fermentum a, aliquet quis,
-    sodales at, dolor. Duis eget velit eget risus fringilla hendrerit.
-    Nulla facilisi. Mauris turpis pede, aliquet ac, mattis sed, consequat
-    in, massa. Cum sociis natoque penatibus et magnis dis parturient
-    montes, nascetur ridiculus mus. Etiam egestas posuere metus. Aliquam
-    erat volutpat. Donec non tortor. Vivamus posuere nisi mollis dolor.
-    Quisque porttitor nisi ac.
+    <Averages />
   </Accordion>
