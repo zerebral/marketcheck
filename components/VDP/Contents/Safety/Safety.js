a7c59ba3f2514b3a4f63e7a5c42cc702da9546d8
Miguel Palau <mpalauzarza@gmail.com>
2017-09-13 09:56:57 -0500

    safety and similar skeleton

---
+++ components/VDP/Contents/Safety/Safety.js
@@ -0,0 +1,16 @@
+import { Accordion } from '~/general'
+
+export default () =>
+  <Accordion title='Safety Ratings' open >
+    Such accordant wow Commodo vitae, aliquam ut, posuere eget, dui.
+    Praesent massa dui, mattis et, vehicula auctor, iaculis id, diam. Morbi
+    viverra neque sit amet risus. Nunc pellentesque aliquam orci. Proin
+    neque elit, mollis vel, tristique nec, varius consectetuer, lorem. Nam
+    malesuada ornare nunc. Duis turpis turpis, fermentum a, aliquet quis,
+    sodales at, dolor. Duis eget velit eget risus fringilla hendrerit.
+    Nulla facilisi. Mauris turpis pede, aliquet ac, mattis sed, consequat
+    in, massa. Cum sociis natoque penatibus et magnis dis parturient
+    montes, nascetur ridiculus mus. Etiam egestas posuere metus. Aliquam
+    erat volutpat. Donec non tortor. Vivamus posuere nisi mollis dolor.
+    Quisque porttitor nisi ac.
+  </Accordion>
--- components/VDP/Contents/index.js
+++ components/VDP/Contents/index.js
@@ -1,7 +1,5 @@
 import { Container } from '~/layout'
 import ContentsWrapper from './ContentsWrapper'
-import { Accordion } from '~/general'
-
 import SummaryReport from './SummaryReport'
 import VehicleInformation from './VehicleInformation'
 import MarketComparison from './MarketComparison'
@@ -13,6 +11,8 @@ import Environmental from './Environmental'
 import ModelPopularity from './ModelPopularity'
 import Competition from './Competition'
 import ResaleValue from './ResaleValue'
+import Safety from './Safety'
+import Similar from './Similar'
 
 export default props =>
   <ContentsWrapper>
@@ -28,31 +28,7 @@ export default props =>
       <ModelPopularity />
       <Competition />
       <ResaleValue />
-      <Accordion title='Safety Ratings'>
-        Such accordant wow Commodo vitae, aliquam ut, posuere eget, dui.
-        Praesent massa dui, mattis et, vehicula auctor, iaculis id, diam. Morbi
-        viverra neque sit amet risus. Nunc pellentesque aliquam orci. Proin
-        neque elit, mollis vel, tristique nec, varius consectetuer, lorem. Nam
-        malesuada ornare nunc. Duis turpis turpis, fermentum a, aliquet quis,
-        sodales at, dolor. Duis eget velit eget risus fringilla hendrerit.
-        Nulla facilisi. Mauris turpis pede, aliquet ac, mattis sed, consequat
-        in, massa. Cum sociis natoque penatibus et magnis dis parturient
-        montes, nascetur ridiculus mus. Etiam egestas posuere metus. Aliquam
-        erat volutpat. Donec non tortor. Vivamus posuere nisi mollis dolor.
-        Quisque porttitor nisi ac.
-      </Accordion>
-      <Accordion title='Similar Local Cars'>
-        Such accordant wow Commodo vitae, aliquam ut, posuere eget, dui.
-        Praesent massa dui, mattis et, vehicula auctor, iaculis id, diam. Morbi
-        viverra neque sit amet risus. Nunc pellentesque aliquam orci. Proin
-        neque elit, mollis vel, tristique nec, varius consectetuer, lorem. Nam
-        malesuada ornare nunc. Duis turpis turpis, fermentum a, aliquet quis,
-        sodales at, dolor. Duis eget velit eget risus fringilla hendrerit.
-        Nulla facilisi. Mauris turpis pede, aliquet ac, mattis sed, consequat
-        in, massa. Cum sociis natoque penatibus et magnis dis parturient
-        montes, nascetur ridiculus mus. Etiam egestas posuere metus. Aliquam
-        erat volutpat. Donec non tortor. Vivamus posuere nisi mollis dolor.
-        Quisque porttitor nisi ac.
-      </Accordion>
+      <Safety />
+      <Similar />
     </Container>
   </ContentsWrapper>
