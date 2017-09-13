1db1e098e9e3d7dfef06fb486d3a1688167c8877
Miguel Palau <mpalauzarza@gmail.com>
2017-09-13 12:33:05 -0500

    more compact rating component

--- components/VDP/Contents/Safety/Safety.js
+++ components/VDP/Contents/Safety/Safety.js
@@ -1,16 +1,18 @@
 import { Accordion } from '~/general'
+import styled from 'styled-components'
+import Rating from './Rating'
+
+const Summary = styled.p`
+  opacity: 0.56;
+  margin-bottom: 45px !important;
+`
 
 export default () =>
   <Accordion title='Safety Ratings' open >
-    Such accordant wow Commodo vitae, aliquam ut, posuere eget, dui.
-    Praesent massa dui, mattis et, vehicula auctor, iaculis id, diam. Morbi
-    viverra neque sit amet risus. Nunc pellentesque aliquam orci. Proin
-    neque elit, mollis vel, tristique nec, varius consectetuer, lorem. Nam
-    malesuada ornare nunc. Duis turpis turpis, fermentum a, aliquet quis,
-    sodales at, dolor. Duis eget velit eget risus fringilla hendrerit.
-    Nulla facilisi. Mauris turpis pede, aliquet ac, mattis sed, consequat
-    in, massa. Cum sociis natoque penatibus et magnis dis parturient
-    montes, nascetur ridiculus mus. Etiam egestas posuere metus. Aliquam
-    erat volutpat. Donec non tortor. Vivamus posuere nisi mollis dolor.
-    Quisque porttitor nisi ac.
+    <Summary>Define Where Data is Coming From</Summary>
+    <Rating stars={4} name='Overall' />
+    <Rating stars={2} name='Roll Over' />
+    <Rating stars={3.2} name='Driver Side' />
+    <Rating stars={4.3} name='Driver Front' />
+    <Rating stars={5} name='Passenger Front' />
   </Accordion>
