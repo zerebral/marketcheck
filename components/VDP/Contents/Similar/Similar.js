619ac8136d45183fc40eece1486d20ba3fc87adb
Miguel Palau <mpalauzarza@gmail.com>
2017-09-14 10:28:19 -0500

    initial data for the similar cars component

--- components/VDP/Contents/Similar/Similar.js
+++ components/VDP/Contents/Similar/Similar.js
@@ -1,16 +1,14 @@
 import { Accordion } from '~/general'
+import styled from 'styled-components'
+import { number } from '%/format'
+import data from './seedData.js'
+
+const Summary = styled.p`
+  opacity: 0.56;
+  margin-bottom: 30px !important;
+`
 
 export default () =>
-  <Accordion title='Similar Local Cars'>
-    Such accordant wow Commodo vitae, aliquam ut, posuere eget, dui.
-    Praesent massa dui, mattis et, vehicula auctor, iaculis id, diam. Morbi
-    viverra neque sit amet risus. Nunc pellentesque aliquam orci. Proin
-    neque elit, mollis vel, tristique nec, varius consectetuer, lorem. Nam
-    malesuada ornare nunc. Duis turpis turpis, fermentum a, aliquet quis,
-    sodales at, dolor. Duis eget velit eget risus fringilla hendrerit.
-    Nulla facilisi. Mauris turpis pede, aliquet ac, mattis sed, consequat
-    in, massa. Cum sociis natoque penatibus et magnis dis parturient
-    montes, nascetur ridiculus mus. Etiam egestas posuere metus. Aliquam
-    erat volutpat. Donec non tortor. Vivamus posuere nisi mollis dolor.
-    Quisque porttitor nisi ac.
+  <Accordion title='Similar Local Cars' open >
+    <Summary>{number(data.num_found)} Similar cars in Your Local Area</Summary>
   </Accordion>
