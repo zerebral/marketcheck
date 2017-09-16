ade6f22921c62eeeebb4b4e5690b30300c507089
Luis Antonio Gomez <jimmylagp@gmail.com>
2017-09-15 19:58:55 -0500

    Building component

--- components/SRP/Contents/Sidebar/RatingStars/index.js
+++ components/SRP/Contents/Sidebar/RatingStars/index.js
@@ -1,35 +1,3 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import { FlexRow, FlexCol } from '~/layout'
-import { rem, em } from 'polished'
-import { colors } from '%/styles'
-import Collapsible from '../Collapsible'
-import StarsControl from './StarsControl'
+import Rating from './Rating.js'
 
-
-class RatingStars extends Component{
-  constructor(props){
-    super(props);
-
-    this.label = this.props.label
-
-    this.state = {
-      rating: 1
-    };
-
-  }
-
-  onStarClick(nextValue, prevValue, name) {
-    this.setState({rating: nextValue});
-  }
-
-  render(){
-    return (
-      <Collapsible label={this.label}>
-        <StarsControl />
-      </Collapsible>
-    )
-  }
-}
-
-export default RatingStars;
+export default Rating
