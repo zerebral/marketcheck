c16f5a00a018bcecd842123eb8c068437d9ce11e
Miguel Palau <mpalauzarza@gmail.com>
2017-09-20 19:41:37 -0500

    Safety done

--- components/VDP/Desktop/Contents/Safety/Safety.js
+++ components/VDP/Desktop/Contents/Safety/Safety.js
@@ -1,19 +1,23 @@
 import Section from '~/VDP/Section'
 import Rating from './Rating'
 import Seller from './Seller'
-import { FlexRow } from '~/layout'
+import { FlexRow, FlexCol } from '~/layout'
 
 export default () =>
   <Section title='Safety Ratings' summary='Define Where Data is Coming From'>
-    <FlexRow justify='center' noMargin >
-      <Rating stars={4} name='Overall' />
-    </FlexRow>
 
-    <FlexRow justify='center' noMargin >
-      <Rating stars={2} name='Roll Over' />
-      <Rating stars={3.2} name='Driver Side' />
-      <Rating stars={4.3} name='Driver Front' />
-      <Rating stars={5} name='Passenger Front' />
+    <FlexRow justify='center'>
+      <FlexCol cols={8}>
+        <FlexRow justify='center' noMargin >
+          <Rating stars={4} name='Overall' />
+        </FlexRow>
+        <FlexRow justify='center' noMargin >
+          <Rating stars={2} name='Roll Over' />
+          <Rating stars={3.2} name='Driver Side' />
+          <Rating stars={4.3} name='Driver Front' />
+          <Rating stars={5} name='Passenger Front' />
+        </FlexRow>
+      </FlexCol>
     </FlexRow>
 
     <Seller
