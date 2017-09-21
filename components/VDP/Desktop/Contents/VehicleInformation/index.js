667c8dcd4510ddae0b6dd8e2c50e3fc443322b55
Miguel Palau <mpalauzarza@gmail.com>
2017-09-21 15:11:41 -0500

    VehicleInformation done

--- components/VDP/Desktop/Contents/VehicleInformation/index.js
+++ components/VDP/Desktop/Contents/VehicleInformation/index.js
@@ -18,26 +18,37 @@ const Intro = styled.h6`
   font-weight: 400;
 `
 
+const Contents = styled.div`
+  columns: 2;
+
+  p {
+    margin-bottom: 20px;
+  }
+`
+
 export default () =>
   <Section title='Vehicle Information'>
     <Wrapper>
       <Intro>Raw performance, agility, unparalleled ride and handling. These are attributes that people have come to expect from Lotus and the Exige Sport 350 is no exception. Now also available as a roadster - the lightest possible incarnation of the current generation Exige.</Intro>
 
-      <p>Type <strong>New</strong></p>
-      <p>Model <strong>Tiguan</strong></p>
-      <p>Trim <strong>Wolfsburg</strong></p>
-      <p>Body <strong>4D Sport Utility</strong></p>
-      <p>Exterior <strong>Pacific Blue Metallic</strong></p>
-      <p>Interior <strong>Charcoal Black</strong></p>
-      <p>Doors <strong>4</strong></p>
-      <p>Stock# <strong>18156</strong></p>
-      <p>Model # <strong>5N2WV1</strong></p>
-      <p>VIN # <strong>WVGRV7AX9HW500689</strong></p>
-      <p>Mileage <strong>6487</strong></p>
-      <p>Engine <strong>2.0L TSI DOHC</strong></p>
-      <p>Transmission <strong>6-Speed Automatic</strong></p>
-      <p>Fuel <strong>Gasoline</strong></p>
-      <p>Drive <strong>FWD</strong></p>
-      <p>MPG <strong>20 City / 24 Hwy</strong></p>
+      <Contents>
+        <p>Type <strong>New</strong></p>
+        <p>Model <strong>Tiguan</strong></p>
+        <p>Trim <strong>Wolfsburg</strong></p>
+        <p>Body <strong>4D Sport Utility</strong></p>
+        <p>Exterior <strong>Pacific Blue Metallic</strong></p>
+        <p>Interior <strong>Charcoal Black</strong></p>
+        <p>Doors <strong>4</strong></p>
+        <p>Stock# <strong>18156</strong></p>
+        <p>Model # <strong>5N2WV1</strong></p>
+        <p>VIN # <strong>WVGRV7AX9HW500689</strong></p>
+        <p>Mileage <strong>6487</strong></p>
+        <p>Engine <strong>2.0L TSI DOHC</strong></p>
+        <p>Transmission <strong>6-Speed Automatic</strong></p>
+        <p>Fuel <strong>Gasoline</strong></p>
+        <p>Drive <strong>FWD</strong></p>
+        <p>MPG <strong>20 City / 24 Hwy</strong></p>
+      </Contents>
+
     </Wrapper>
   </Section>
