2f62cb5ff8150a2dc6782e8e9da8127fd3318211
Jonathan Perez <jonathan.phz@gmail.com>
2017-10-12 18:55:13 -0300

    create fetch for vdp listing api call

--- pages/vdp.js
+++ pages/vdp.js
@@ -2,9 +2,37 @@ import React from 'react'
 import VDP from '@/VDP'
 import FontsHOC from '@/HOC/Fonts'
 
+
 class Vdp extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      vdp: {},
+    }
+  }
+
+  componentDidMount() {
+
+    fetch(`http://marketcheck-prod.apigee.net/v1/listing/ab2281d2-05fc-4d61-aa32-3fab037501c3?api_key=${process.env.API_VAR}`)
+    .then(response => {
+      if ( response.status !== 200) {
+        console.log('Problem ' + response.status)
+      }
+      return response.json();
+    }).then(data => {
+      this.setState({ vdp: data});
+    }).catch( error => {
+      console.log('error message: ' + error.message)
+    })
+
+  }
+
   render () {
-    return <VDP />
+    if (!this.state.vdp.build) {
+      return <div>Loading</div>
+    }
+    return <VDP {...this.state}/>
   }
 }
 
