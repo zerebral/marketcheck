a57fb4f81f1761dd230b767741c85dd942fc9c3a
Jonathan Perez <jonathan.phz@gmail.com>
2017-10-12 18:54:38 -0300

    add api props

--- containers/VDP/index.js
+++ containers/VDP/index.js
@@ -1,5 +1,11 @@
 import VDP from '~/VDP'
 import vdpdata from './vdpdata'
 
-export default () =>
-  <VDP {...vdpdata} />
+export default (props) =>
+  <VDP 
+  {...vdpdata} 
+  {...props} 
+  name={`${props.vdp.build.make} ${props.vdp.build.model}`} 
+  brief={`Stock # ${props.vdp.stock_no}, ${props.vdp.build.engine}, ${props.vdp.build.transmission}, ${props.vdp.miles} mi., ${parseInt(props.vdp.build.city_miles)}/${parseInt(props.vdp.build.highway_miles)} MPG*`}
+  price={props.vdp.price}
+  />
