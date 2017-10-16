acb3cf71328f76c1d8a42b45bd05b6053e07472f
Jonathan Perez <jonathan.phz@gmail.com>
2017-10-16 10:05:29 -0300

    deconstruct and rename a few props

--- containers/VDP/index.js
+++ containers/VDP/index.js
@@ -1,11 +1,49 @@
 import VDP from '~/VDP'
 import vdpdata from './vdpdata'
 
-export default (props) =>
+export default ({
+  price,
+  miles,
+  vin,
+  inventory_type: type,
+  stock_no: stockNumber,
+  exterior_color: exteriorColor,
+  interior_color: interiorColor,
+  build: {
+    make,
+    model,
+    engine,
+    transmission,
+    drivetrain,
+    doors,
+    trim,
+    city_miles: cityMiles,
+    highway_miles: highwayMiles,
+    body_type: body,
+    fuel_type: fuel,
+  }
+}) =>
+
   <VDP 
   {...vdpdata} 
-  {...props} 
-  name={`${props.vdp.build.make} ${props.vdp.build.model}`} 
-  brief={`Stock # ${props.vdp.stock_no}, ${props.vdp.build.engine}, ${props.vdp.build.transmission}, ${props.vdp.miles} mi., ${parseInt(props.vdp.build.city_miles)}/${parseInt(props.vdp.build.highway_miles)} MPG*`}
-  price={props.vdp.price}
+  //{...props} 
+  name={`${make} ${model}`}
+  brief={`Stock # ${stockNumber}, ${engine}, ${transmission}, ${miles} mi., ${parseInt(cityMiles)}/${parseInt(highwayMiles)} MPG*`}
+  price={price}
+  model={model}
+  type={type}
+  stockNumber={stockNumber}
+  trim={trim}
+  body={body}
+  exteriorColor={exteriorColor}
+  interiorColor={interiorColor}
+  doors={doors}
+  vin={vin}
+  miles={miles}
+  engine={engine}
+  transmission={transmission}
+  fuel={fuel}
+  drivetrain={drivetrain}
+  cityMiles={cityMiles}
+  highwayMiles={highwayMiles}
   />
