cf179c6a28e99b739520460382c34c325d462899
Jonathan Perez <jonathan.phz@gmail.com>
2017-10-19 19:08:39 -0300

    Create a non working AveragesOverTime Container

--- containers/VDP/AveragesOverTime/AveragesOverTimeContainer.js
+++ containers/VDP/AveragesOverTime/AveragesOverTimeContainer.js
@@ -1,16 +1,59 @@
 import React, { Component } from 'react';
-import AveragesOverTimeDesktop from '~/Desktop/Contents/AveragesOverTime';
-import AveragesOverTimeMobile from '~/Mobile/Contents/AveragesOverTime';
+import AveragesOverTimeDesktop from '~/VDP/Desktop/Contents/AveragesOverTime/AveragesOverTime';
+//import AveragesOverTimeMobile from '~/Mobile/Contents/AveragesOverTime/AveragesOverTime';
+import fetch from 'isomorphic-fetch';
 
 class AveragesOverTimeContainer extends Component {
-  fjhdkjfhajkdfds
+  constructor(props) {
+    super(props);
+
+    this.trendsFetch = this.trendsFetch.bind(this);
+
+    this.state = {
+      trends: [],
+      testing: 'testing from container'
+    }
+  }
+  
+  trendsFetch(url) {
+    fetch(url)
+      .then(response => {
+        if (response.status !== 200) {
+          console.log('Problem ' + response.status)
+        }
+        return response.json();
+      }).then(data => {
+        if (data.trends) {
+
+          //Convert number strings to actual numbers in Obj
+          for (var i = 0; i < data.trends.length; i++) {
+            var obj = data.trends[i];
+            for (var prop in obj) {
+              if (obj.hasOwnProperty(prop) && obj[prop] !== null && !isNaN(obj[prop])) {
+                obj[prop] = parseInt(+obj[prop]);
+              }
+            }
+          }
+
+          data.trends.forEach(item => {
+            item['name'] = item['month'] + "/" + item['year'];
+          });
+          //console.log(JSON.stringify(data.trends, null, 2));
+          this.setState({ trends: data.trends.reverse() });
+        }
+
+      }).catch(error => {
+        console.log('error message: ' + error.message)
+      })
+  }
+
+  componentDidMount() {
+    console.log('container');
+    //this.trendsFetch(`http://${process.env.API_HOST}/v1/trends?api_key=${process.env.API_VAR}&vin=1FMCU9GXXEUA10071&car_type=used`)
+  }
+
   render() {
-    return (
-      <Aux>
-        <AveragesOverTimeDesktop/>
-        <AveragesOverTimeMobile />
-      </Aux>
-    );
+    return   <AveragesOverTimeDesktop {...this.state} testing="from container"/>
   }
 }
 
