7e7fd0eec63c5d32d9de562d878d871c3adcc366
Jonathan Perez <jonathan.phz@gmail.com>
2017-10-23 18:43:34 -0300

    remove dummy Table components and pass parent props

--- components/VDP/Desktop/Contents/Competition/Competition.js
+++ components/VDP/Desktop/Contents/Competition/Competition.js
@@ -1,9 +1,7 @@
 import Section from '~/VDP/Section'
 import { Table } from '~/general'
 
-export default () =>
-  <Section title='The Competition' summary='Similar cars currently for sale'>
-    <Table />
-    <Table />
-    <Table />
+export default (props) =>
+  <Section title='The Competition' summary='Similar cars currently for sale' className={props.className} {...props}>
+    {props.similarCompetition && <Table data={props.similarCompetition} />}
   </Section>
