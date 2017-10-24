1cec2fac1334f7a98ef829231b38722d9ef6cd81
Jonathan Perez <jonathan.phz@gmail.com>
2017-10-24 20:06:32 -0300

    pass new props and add a second table

--- components/VDP/Desktop/Contents/Competition/Competition.js
+++ components/VDP/Desktop/Contents/Competition/Competition.js
@@ -1,7 +1,13 @@
 import Section from '~/VDP/Section'
 import { Table } from '~/general'
+import Aux from 'react-aux';
 
 export default (props) =>
   <Section title='The Competition' summary='Similar cars currently for sale' className={props.className} {...props}>
-    {props.similarCompetition && <Table data={props.similarCompetition} />}
+    {props.similarCompetition && 
+      <Aux>
+        <Table data={props.competition.same_car_models} headings={['Same Car Model', 'Avg. Price', 'Avg. Mileage', 'Avg. Market Value']}/>
+        <Table data={props.competition.similar_car_models} headings={['Similar Car Models', 'Avg. Price', 'Avg. Mileage', 'Avg. Market Value']}/>
+      </Aux>
+    }
   </Section>
