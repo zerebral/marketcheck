dfcc0914dcc2951d82f147a99664e2dc1ae224b5
Jonathan Perez <jonathan.phz@gmail.com>
2017-10-26 18:56:29 -0300

    populate resale value table

--- components/VDP/Desktop/Contents/ResaleValue/ResaleValue.js
+++ components/VDP/Desktop/Contents/ResaleValue/ResaleValue.js
@@ -1,12 +1,17 @@
 import Section from '~/VDP/Section'
 import LineChart from './LineChart.js'
 import Similar from './Similar.js'
+import Aux from 'react-aux';
 
-export default () =>
+export default ({ resaleValue }) =>
   <Section title='Model Resale Value' summary='Based upon avg. depreciation rates'>
-    <Similar leftText='Similar Models' rightText='Current Value' />
-    <Similar rightText='1 Year From Now' />
-    <Similar rightText='2 Years From Now' />
-    <Similar rightText='5 Years From Now' />
-    <LineChart />
+    {resaleValue  && 
+    <Aux>
+      <Similar leftText='Similar Models' rightText='Current Value' resaleValue={resaleValue} timeValue="current_value"/>
+      <Similar rightText='1 Year From Now' resaleValue={resaleValue} timeValue="one_year_from_now"/>
+      <Similar rightText='2 Years From Now' resaleValue={resaleValue} timeValue="two_year_from_now"/>
+      <Similar rightText='5 Years From Now' resaleValue={resaleValue} timeValue="five_year_from_now"/>
+      <LineChart />
+    </Aux>
+    }
   </Section>
