7f2c236e0902a6cf534ce166cff41253b92bec77
Jonathan Perez <jonathan.phz@gmail.com>
2017-10-31 16:35:15 -0300

    remove commented out code

--- components/VDP/Mobile/Contents/ModelPopularity/LineCharts.js
+++ components/VDP/Mobile/Contents/ModelPopularity/LineCharts.js
@@ -10,22 +10,4 @@ export default (props) =>
     {props.modelPopularity.map((item, key) => {
       return <SingleChart value={item.left} remaining={item.right} we={item.text} key={key} />
     })}
-    {/* <SingleChart
-      value={51}
-      remaining={68}
-      we='Toyota'
-      them='Other Manufacturers'
-    />
-    <SingleChart
-      value={112}
-      remaining={73}
-      we='Avalon'
-      them='Other Sedans'
-    />
-    <SingleChart
-      value={40}
-      remaining={51}
-      we='2000 Toyota'
-      them='Other Model Years'
-    /> */}
   </ChartsWrapper>
