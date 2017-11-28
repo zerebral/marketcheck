c9bb0c384992813b9cfb3256a1e312a92cfb5f4c
Luis Antonio Gomez <jimmylagp@gmail.com>
2017-11-28 13:45:21 -0600

    Fix 404

--- components/VDP/Desktop/Overview/Slideshow/index.js
+++ components/VDP/Desktop/Overview/Slideshow/index.js
@@ -2,6 +2,7 @@ import SliderWrapper from './SlideWrapper'
 import Slider from 'react-slick'
 import Slide from './Slide'
 import SliderHOC from '@/HOC/Slider'
+import check404 from '%/factory/check404'
 
 import { NextArrow, PreviousArrow } from './Components.js'
 
@@ -13,15 +14,41 @@ const SliderSettings = {
   prevArrow: <PreviousArrow />
 }
 
-const OverviewSlider = ({slides}) =>
-  <SliderWrapper>
-    <Slider {...SliderSettings}>
-      {slides.map((slide, index) =>
-        <div key={index} >
-          <Slide image={slide} />
-        </div>
-      )}
-    </Slider>
-  </SliderWrapper>
+const OverviewSlider = ({slides}) => {
+
+  if (slides.length > 0) {
+    return (
+      <SliderWrapper>
+        <Slider {...SliderSettings}>
+          {slides.map((slide, index) => {
+            if (check404(slide)) {
+              return (
+                <div key={index} >
+                  <Slide image={slide} />
+                </div>
+              )
+            } else {
+              return (
+                <div key={index} >
+                  <Slide image="/static/img/vdp/notfound.jpg" />
+                </div>
+              )
+            }
+          })}
+        </Slider>
+      </SliderWrapper>
+    )
+  } else {
+    return (
+      <SliderWrapper>
+        <Slider {...SliderSettings}>
+          <div>
+            <Slide image="/static/img/vdp/notfound.jpg" />
+          </div>
+        </Slider>
+      </SliderWrapper>
+    )
+  }
+}
 
 export default SliderHOC(OverviewSlider)
