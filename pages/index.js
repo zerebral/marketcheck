641a2f786382289e4f4fb5340d357bc66533b795
dsilvaaulison <aulison@zerebral.co.in>
2018-01-22 18:10:26 +0530

    lin redirect to vdp page

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,30 @@ import Home from '@/Home'
 import FontsHOC from '@/HOC/Fonts'
 
 class Index extends React.Component {
+    constructor(props) {
+    super(props)
+    this.setVin = this.setVin.bind(this)
+    this.state = {
+        vin: ''
+    }
+}
+
+    setVin () {
+        console.log(this.props.url.query.vin)
+        console.log(this)
+        // this.urlParams.vin = value
+        if(this.props.url.query.vin) {
+            this.setState({
+                vin: this.props.url.query.vin
+            }, () => {
+                window.location.href = "/vdp/" + this.props.url.query.vin;
+            })
+        }
+
+    }
+
   componentDidMount () {
+    this.setVin()
     if (!window.GA_INITIALIZED) {
       initGA()
       window.GA_INITIALIZED = true
